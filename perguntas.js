90910e264c6586f43c7ed73d46a719e7669bb0ee
Luizlx6 <138134836+Luizlx6@users.noreply.github.com>
2024-10-28 08:27:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Saúde',
+    'Quais são os benefícios do exercício?',
+    'Melhora o humor e a saúde cardiovascular'
+)
+
+criaCartao(
+    'Meio Ambiente',
+    'O que causa a poluição do ar?',
+    'Causada por veículos e indústrias'
+)
+
+criaCartao(
+    'Tecnologia',
+    'Como funciona a energia solar? ',
+    'Converte luz solar em eletricidade'
+)
+
+criaCartao(
+    'Psicologia',
+    'Quais são os efeitos do estresse?    ',
+    'Causa ansiedade e fadiga'
+)
